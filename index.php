dc084150f029a829dba8fa7fb4fc6855229f603f
RafRamdan <rafiramdan2310@gmail.com>
2023-07-31 09:46:44 +0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello world</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque culpa eius facere beatae non, minus ut, dolores hic voluptatem numquam sequi tempore quibusdam maiores modi ipsa eaque doloremque maxime impedit?</p>
 </body>
 </html>
